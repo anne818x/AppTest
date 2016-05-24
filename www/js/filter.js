2bffb22d0935b418f00506247580e0427b1fd8ed
zulheika28 <zulheika_batista@hotmail.com>
2016-05-24 11:26:46 +0200

    double footer gone

--- www/js/filter.js
+++ www/js/filter.js
@@ -95,15 +95,6 @@ function appendArrayToApp4(array)
   $("#allVacancies").empty();
     
     showDialogScreen();
-      $("#footerLine").append("<div id='footer'></div>");
-
-  $("#footer").append("<div id='email'><input id='inputEmail' class='input' onclick='removeValue(this.id);' type='email' value='Jouw emailadres'></input></div>");
-  $("#footer").append("<div id='phone'><input id='inputPhone' class='input' onclick='removeValue(this.id);' type='tel' value='Jouw telefoonnummer'></input></div>");
-  $("#footer").append("<div id='reactButton'><img onclick='checkCorrectInput();' src='img/react.png' alt='react'/></div>");
-    $("#footerLine").hide();
-    $("#chosenVacancy").hide();
-    
-    $("#filterbox").show();
 
 $("#internal-popup").hide();
   for(i = 0; i < array.length; i++)
@@ -154,7 +145,7 @@ $("#internal-popup").hide();
         readAll(vacancy);
       });
   }
-};
+}
       
 function appendAllVacancies(array)
 {
@@ -219,4 +210,4 @@ function appendAllVacancies(array)
   }
 
           
-};
+}
