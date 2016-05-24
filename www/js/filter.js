81749f2c9d88e229098de3008833ed1301542e58
anne818x <annemarieke-m@hotmail.com>
2016-05-24 11:06:57 +0200

    vacancies back

--- www/js/filter.js
+++ www/js/filter.js
@@ -54,6 +54,14 @@ function filter6()
   });
 };
 
+function filterFull()
+{
+    $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+    jsonToFullArray(result);
+  });
+}
+
 // Vertaal het JSON object, ontvangen in read, naar een Javascript array.
 function jsonToArray(result)
 {
@@ -68,13 +76,32 @@ function jsonToArray(result)
   appendArrayToApp4(array);
 };
 
+function jsonToFullArray(result)
+{
+  var array = [];
+  for(key in result)
+  {
+    if(result.hasOwnProperty(key))
+    {
+      array.push(result[key]);
+    }
+  }
+  appendAllVacancies(array);
+};
+
 // Voor iedere vacancyTitle in de Javascript array: maak een nieuw div element aan.
 function appendArrayToApp4(array)
 {
-  $("#allVacancies").empty();
-    showDialogScreen();
-  $("#internal-popup").hide();
+  showDialogScreen();
+      $("#footerLine").append("<div id='footer'></div>");
+
+  $("#footer").append("<div id='email'><input id='inputEmail' class='input' onclick='removeValue(this.id);' type='email' value='Jouw emailadres'></input></div>");
+  $("#footer").append("<div id='phone'><input id='inputPhone' class='input' onclick='removeValue(this.id);' type='tel' value='Jouw telefoonnummer'></input></div>");
+  $("#footer").append("<div id='reactButton'><img onclick='checkCorrectInput();' src='img/react.png' alt='react'/></div>");
+    $("#footerLine").hide();
+    $("#chosenVacancy").hide();
     
+    $("#filterbox").show();
 
   for(i = 0; i < array.length; i++)
   {
@@ -123,6 +150,69 @@ function appendArrayToApp4(array)
         vacancy = $(document.elementFromPoint(xMousePoint, yMousePoint)).text();
         readAll(vacancy);
       });
-    
   }
+};
+      
+function appendAllVacancies(array)
+{
+    $("#allVacancies").empty();
+    $("#internal-popup").hide();
+    showDialogScreen();
+      $("#footerLine").append("<div id='footer'></div>");
+
+  $("#footer").append("<div id='email'><input id='inputEmail' class='input' onclick='removeValue(this.id);' type='email' value='Jouw emailadres'></input></div>");
+  $("#footer").append("<div id='phone'><input id='inputPhone' class='input' onclick='removeValue(this.id);' type='tel' value='Jouw telefoonnummer'></input></div>");
+  $("#footer").append("<div id='reactButton'><img onclick='checkCorrectInput();' src='img/react.png' alt='react'/></div>");
+    $("#footerLine").hide();
+    $("#chosenVacancy").hide();
+    
+    $("#filterbox").show();
+  for(i = 0; i < array.length; i++)
+  {
+    if(i % 2 == 0)
+    {
+      $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+
+      if(array[i + 1] == "1")
+      {
+        $("#vacancy" + i).css("background-color", "#c7d320");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-06.png' alt='icon'/>");
+      }
+      else if(array[i + 1] == "2")
+      {
+        $("#vacancy" + i).css("background-color", "#1b365d");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-01.png' alt='icon'/>");
+      }
+      else if(array[i + 1] == "3")
+      {
+        $("#vacancy" + i).css("background-color", "#eb660a");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-02.png' alt='icon'/>");
+      }
+      else if(array[i + 1] == "4")
+      {
+        $("#vacancy" + i).css("background-color", "#e3251a");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-03.png' alt='icon'/>");
+      }
+      else if(array[i + 1] == "5")
+      {
+        $("#vacancy" + i).css("background-color", "#0c8ece");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-04.png' alt='icon'/>");
+      }
+      else if(array[i + 1] == "6")
+      {
+        $("#vacancy" + i).css("background-color", "#ffda05");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
+      }
+
+      $("#vacancy" + i).click(function()
+      {
+        xMousePoint = event.clientX;
+        yMousePoint = event.clientY;
+        vacancy = $(document.elementFromPoint(xMousePoint, yMousePoint)).text();
+        readAll(vacancy);
+      });
+    }
+  }
+
+          
 };
