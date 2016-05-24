2ca9a14300e9cfcb19d818aa3c966ef048af7c0a
anne818x <annemarieke-m@hotmail.com>
2016-05-24 10:34:51 +0200

    filters work

--- www/js/filter.js
+++ www/js/filter.js
@@ -1,14 +1,61 @@
-// Ontvang vacancyTitle en categoryID, van read.php als JSON object.
-function filter()
+var number;
+
+function filter1()
 {
   $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
   {
-    jsonToArray2(result);
+      number = 1;
+    jsonToArray(result);
+  });
+};
+
+function filter2()
+{
+  $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+      number = 2;
+    jsonToArray(result);
+  });
+};
+
+function filter3()
+{
+  $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+      number = 3;
+    jsonToArray(result);
+  });
+};
+
+function filter4()
+{
+  $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+      number = 4;
+    jsonToArray(result);
+  });
+};
+
+function filter5()
+{
+  $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+      number = 5;
+    jsonToArray(result);
+  });
+};
+
+function filter6()
+{
+  $.getJSON("http://uwvproject.serverict.nl/App/read.php", function(result)
+  {
+      number = 6;
+    jsonToArray(result);
   });
 };
 
 // Vertaal het JSON object, ontvangen in read, naar een Javascript array.
-function jsonToArray2(result)
+function jsonToArray(result)
 {
   var array = [];
   for(key in result)
@@ -18,32 +65,56 @@ function jsonToArray2(result)
       array.push(result[key]);
     }
   }
-  appendArrayToApp2(array);
+  appendArrayToApp4(array);
 };
 
 // Voor iedere vacancyTitle in de Javascript array: maak een nieuw div element aan.
-function appendArrayToApp2(array)
+function appendArrayToApp4(array)
 {
   $("#allVacancies").empty();
     showDialogScreen();
-      $("#footerLine").append("<div id='footer'></div>");
-
-  $("#footer").append("<div id='email'><input id='inputEmail' class='input' onclick='removeValue(this.id);' type='email' value='Jouw emailadres'></input></div>");
-  $("#footer").append("<div id='phone'><input id='inputPhone' class='input' onclick='removeValue(this.id);' type='tel' value='Jouw telefoonnummer'></input></div>");
-  $("#footer").append("<div id='reactButton'><img onclick='checkCorrectInput();' src='img/react.png' alt='react'/></div>");
-    $("#footerLine").hide();
-    $("#chosenVacancy").hide();
+  $("#internal-popup").hide();
     
-    $("#filterbox").show();
+
   for(i = 0; i < array.length; i++)
   {
-    if(array[i + 1] == "1")
-    {
-      $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
-
+      if(number === 1 && array[i + 1] === "1")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
         $("#vacancy" + i).css("background-color", "#c7d320");
         $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-06.png' alt='icon'/>");
-      
+      }
+      else if(number === 2 && array[i + 1] === "2")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+        $("#vacancy" + i).css("background-color", "#1b365d");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-01.png' alt='icon'/>");
+      }
+      else if(number === 3 && array[i + 1] == "3")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+        $("#vacancy" + i).css("background-color", "#eb660a");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-02.png' alt='icon'/>");
+      }
+      else if(number === 4 && array[i + 1] == "4")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+        $("#vacancy" + i).css("background-color", "#e3251a");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-03.png' alt='icon'/>");
+      }
+      else if(number === 5 && array[i + 1] == "5")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+        $("#vacancy" + i).css("background-color", "#0c8ece");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-04.png' alt='icon'/>");
+      }
+      else if(number === 6 && array[i + 1] == "6")
+      {
+          $("#allVacancies").append("<div id='vacancy"+ i +"' class='vacancy text'>"+ array[i] + "</div>");
+        $("#vacancy" + i).css("background-color", "#ffda05");
+        $("#vacancy" + i).prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
+      }
+  
 
       $("#vacancy" + i).click(function()
       {
@@ -52,6 +123,6 @@ function appendArrayToApp2(array)
         vacancy = $(document.elementFromPoint(xMousePoint, yMousePoint)).text();
         readAll(vacancy);
       });
-    }
+    
   }
 };
