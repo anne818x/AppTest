8ee65b6d15958f1ea70f9c0bb95da81e5c6461b1
Emiilly <emilytimmerman@hotmail.co.uk>
2016-06-03 10:34:36 +0200

    changes to readall

--- UWV_Rene/UWV_APP_local/AppTest/www/js/readAll.js
+++ UWV_Rene/UWV_APP_local/AppTest/www/js/readAll.js
@@ -53,62 +53,62 @@ function appendToDetailsPage(vacancy, description, requirements, hours, branch)
   if(branch == "Zorg")
   {
     $("#vacancy").css("background-color", "#c7d320");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-03.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-06.png' alt='icon'/>");
   }
-  if(branch == "zorg")
+  else if(branch == "zorg")
   {
     $("#vacancy").css("background-color", "#c7d320");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-03.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-06.png' alt='icon'/>");
   }
   else if(branch == "ICT")
   {
     $("#vacancy").css("background-color", "#1b365d");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-01.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-01.png' alt='icon'/>");
   }
   else if(branch == "Horeca")
   {
-    $("#vacancy").css("background-color", "#eb660a");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-02.png' alt='icon'/>");
+     $("#vacancy").css("background-color", "#eb660a");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-02.png' alt='icon'/>");
   }
   else if(branch == "Agrarische Sector")
   {
-    $("#vacancy").css("background-color", "#e3251a");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-03.png' alt='icon'/>");
+     $("#vacancy").css("background-color", "#e3251a");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-03.png' alt='icon'/>");
   }
   else if(branch == "Ambacht")
   {
     $("#vacancy").css("background-color", "#0c8ece");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-04.png' alt='icon'/>");
+        $("#vacancy" ).prepend("<img class='icon' src='img/Icons-04.png' alt='icon'/>");
   }
   else if(branch == "Beveiliging")
   {
-    $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+    $("#vacancy" ).css("background-color", "#ffda05");
+        $("#vacancy" ).prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
   else if(branch == "Industrie & Techniek")
   {
     $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
   else if(branch == "Bouw")
   {
     $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
   else if(branch == "Transport en logistiek")
   {
     $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
   else if(branch == "Detailhandel")
   {
     $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
   else if(branch == "Contactcenters")
   {
     $("#vacancy").css("background-color", "#ffda05");
-    $("#vacancy").prepend("<img class='icon2' src='img/Icons-05.png' alt='icon'/>");
+        $("#vacancy").prepend("<img class='icon' src='img/Icons-05.png' alt='icon'/>");
   }
 
   $("#chosenVacancy").append("<div class='text'><h4><u>Branch:</u></h4><p id='infoText'>" + branch + "</p></div>");
